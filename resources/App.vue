d593ed5527593888b4c6e112997fcebdbd248b2e
Carlos <carlos.cruz@cesunbc.edu.mx>
2024-11-04 21:34:20 -0800

    first commit

---
+++ resources/App.vue
@@ -0,0 +1,38 @@
+<template>
+    Hola mundo {{ message }}
+</template>
+<script>
+
+import axios from 'axios';
+
+export default {
+    data() {
+        return {
+            message: '',
+        }
+    },
+    mounted() {
+        this.listener();
+        this.callApi();
+    },
+    methods: {
+        listener() {
+            window.Echo.channel('test')
+            .listen('.notification', (e) => {
+                console.log('escuchando');
+                console.log(e);
+                this.message = e;
+            });
+        },
+        async callApi() {
+            try {
+                const { data } = await axios.get('api/test');
+                console.log('llamando');
+                console.log(data); 
+            } catch (error) {
+                console.error(error);
+            }
+        }
+    }
+}
+</script>
---
+++ resources/js/app.js
@@ -0,0 +1,5 @@
+import './bootstrap';
+import { createApp } from 'vue';
+import App from '../App.vue';
+
+const app = createApp (App).mount('#app')
